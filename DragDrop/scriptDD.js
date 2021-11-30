25bce9c0a578bc34b41ed9884f6febf9cd902898
Liduen26 <raphael.orieux@gmail.com>
2021-11-30 16:18:57 +0100

    Deplacement et resize quasi terminés

--- DragDrop/scriptDD.js
+++ DragDrop/scriptDD.js
@@ -1,47 +1,152 @@
-const elem = document.querySelector(".item");
+const elem = document.querySelectorAll(".depl");
 
 const resizer = document.querySelectorAll(".resizer");
 let currentResizer;
 let isResizing = false;
 
-elem.addEventListener("mousedown", (e) => {
+const movable = document.querySelectorAll(".movable");
 
-    window.addEventListener("mousemove", mousemove);
-    window.addEventListener("mouseup", mouseup);
+const cont = document.querySelector(".container");
 
-    let prevX = e.clientX;
-    let prevY = e.clientY;
+
+//Déplacement -------------------------------------------------------------
+// elem.forEach((item) => {
+//     item.addEventListener("mousedown", (e) => {
+//         console.log(e.target.parentNode);
+//         const targP = e.target.parentNode;
+
+//         window.addEventListener("mousemove", mousemove);
+//         window.addEventListener("mouseup", mouseup);
     
-    
-    function mousemove(e) {
-        // ------ mousedown de Drag and Drop ------
-        if(!isResizing) {
-            let newX = prevX - e.clientX;
-            let newY = prevY - e.clientY;
-
-            const rect = elem.getBoundingClientRect();
-
-            elem.style.left = rect.left - newX + 'px';
-            elem.style.top = rect.top - newY + 'px';
-
-            prevX = e.clientX;
-            prevY = e.clientY;
-        }
+//         let prevX = e.clientX;
+//         let prevY = e.clientY;
         
-    }
+//         function mousemove(e) {
+//             // ------ mousedown de Drag and Drop ------
+//             if(!isResizing) {
+//                 let newX = prevX - e.clientX;
+//                 let newY = prevY - e.clientY;
+    
+//                 const rect = targP.getBoundingClientRect();
+    
+//                 targP.style.left = rect.left - newX + 'px';
+//                 targP.style.top = rect.top - newY + 'px';
+    
+//                 prevX = e.clientX;
+//                 prevY = e.clientY;
+//             }
+            
+//         }
+    
+//         function mouseup() {
+//             window.removeEventListener("mousemove", mousemove);
+//             window.removeEventListener("mouseup", mouseup);
+//         }
+//     });
+// });
 
-    function mouseup() {
-        window.removeEventListener("mousemove", mousemove);
-        window.removeEventListener("mouseup", mouseup);
+cont.addEventListener("mousedown", (e) => {
+    console.log(e.target.parentNode);
+    const targP = e.target.parentNode;
+    if(e.target.classList.contains("depl") || targP.classList.contains("depl")) {
+        
+        window.addEventListener("mousemove", mousemove);
+        window.addEventListener("mouseup", mouseup);
+
+        let prevX = e.clientX;
+        let prevY = e.clientY;
+        
+        function mousemove(e) {
+            // ------ mousedown de Drag and Drop ------
+            if(!isResizing) {
+                let newX = prevX - e.clientX;
+                let newY = prevY - e.clientY;
+
+                const rect = targP.getBoundingClientRect();
+
+                targP.style.left = rect.left - newX + 'px';
+                targP.style.top = rect.top - newY + 'px';
+
+                prevX = e.clientX;
+                prevY = e.clientY;
+            }
+            
+        }
+
+        function mouseup() {
+            window.removeEventListener("mousemove", mousemove);
+            window.removeEventListener("mouseup", mouseup);
+            isResizing = false;
+        }
     }
+    
 });
 
 
+//Resizing -------------------------------------------------------------
+// resizer.forEach((item) => {
+//     item.addEventListener("mousedown", mousedown);
 
-resizer.forEach(item => {
-    item.addEventListener("mousedown", mousedown);
+//     function mousedown(e) {
+//         // ------ mousedown de Resizer ------
+//         currentResizer = e.target;
+//         isResizing = true;
 
-    function mousedown(e) {
+//         let prevX = e.clientX;
+//         let prevY = e.clientY;
+
+//         // console.log(e.target.parentNode);
+//         const targP = e.target.parentNode;
+
+//         window.addEventListener("mousemove", mousemove);
+//         window.addEventListener("mouseup", mouseup);
+
+//         function mousemove(e) {
+            
+//             const rect = targP.getBoundingClientRect();
+
+//             if(currentResizer.classList.contains("se")) {
+//                 //curs bas droite
+//                 targP.style.width = rect.width - (prevX - e.clientX) + "px";
+//                 targP.style.height = rect.height - (prevY - e.clientY) + "px";
+
+//             } else if(currentResizer.classList.contains("sw")) {
+//                 //curs bas gauche
+//                 targP.style.width = rect.width + (prevX - e.clientX) + "px";
+//                 targP.style.height = rect.height - (prevY - e.clientY) + "px";
+
+//                 targP.style.left = rect.left - (prevX - e.clientX) + "px";
+//             } else if(currentResizer.classList.contains("ne")) {
+//                 //curs haut droite
+//                 targP.style.width = rect.width - (prevX - e.clientX) + "px";
+//                 targP.style.height = rect.height + (prevY - e.clientY) + "px";
+
+//                 targP.style.top = rect.top - (prevY - e.clientY) + "px";
+//             } else if(currentResizer.classList.contains("nw")) {
+//                 //curs haut gauche
+//                 targP.style.width = rect.width + (prevX - e.clientX) + "px";
+//                 targP.style.height = rect.height + (prevY - e.clientY) + "px";
+
+//                 targP.style.top = rect.top - (prevY - e.clientY) + "px";
+//                 targP.style.left = rect.left - (prevX - e.clientX) + "px";
+//             }
+
+//             prevX = e.clientX;
+//             prevY = e.clientY;
+//         }
+        
+//         function mouseup() {
+//             window.removeEventListener("mousemove", mousemove);
+//             window.removeEventListener("mouseup", mouseup);
+//             isResizing = false;
+//         }
+//     }
+// });
+
+cont.addEventListener("mousedown", (e) => {
+    console.log(e.target.parentNode);
+    const targP = e.target.parentNode;
+    if(e.target.classList.contains("resizer") || targP.classList.contains("resizer")) {
         // ------ mousedown de Resizer ------
         currentResizer = e.target;
         isResizing = true;
@@ -53,67 +158,34 @@ resizer.forEach(item => {
         window.addEventListener("mouseup", mouseup);
 
         function mousemove(e) {
-            const rect = elem.getBoundingClientRect();
+            
+            const rect = targP.getBoundingClientRect();
 
             if(currentResizer.classList.contains("se")) {
                 //curs bas droite
-                elem.style.width = rect.width - (prevX - e.clientX) + "px";
-                elem.style.height = rect.height - (prevY - e.clientY) + "px";
+                targP.style.width = rect.width - (prevX - e.clientX) + "px";
+                targP.style.height = rect.height - (prevY - e.clientY) + "px";
 
             } else if(currentResizer.classList.contains("sw")) {
                 //curs bas gauche
-                elem.style.width = rect.width + (prevX - e.clientX) + "px";
-                elem.style.height = rect.height - (prevY - e.clientY) + "px";
+                targP.style.width = rect.width + (prevX - e.clientX) + "px";
+                targP.style.height = rect.height - (prevY - e.clientY) + "px";
 
-                elem.style.left = rect.left - (prevX - e.clientX) + "px";
+                targP.style.left = rect.left - (prevX - e.clientX) + "px";
             } else if(currentResizer.classList.contains("ne")) {
                 //curs haut droite
-                elem.style.width = rect.width - (prevX - e.clientX) + "px";
-                elem.style.height = rect.height + (prevY - e.clientY) + "px";
+                targP.style.width = rect.width - (prevX - e.clientX) + "px";
+                targP.style.height = rect.height + (prevY - e.clientY) + "px";
 
-                elem.style.top = rect.top - (prevY - e.clientY) + "px";
+                targP.style.top = rect.top - (prevY - e.clientY) + "px";
             } else if(currentResizer.classList.contains("nw")) {
                 //curs haut gauche
-                elem.style.width = rect.width + (prevX - e.clientX) + "px";
-                elem.style.height = rect.height + (prevY - e.clientY) + "px";
+                targP.style.width = rect.width + (prevX - e.clientX) + "px";
+                targP.style.height = rect.height + (prevY - e.clientY) + "px";
 
-                elem.style.top = rect.top - (prevY - e.clientY) + "px";
-                elem.style.left = rect.left - (prevX - e.clientX) + "px";
+                targP.style.top = rect.top - (prevY - e.clientY) + "px";
+                targP.style.left = rect.left - (prevX - e.clientX) + "px";
             }
-
-            // switch (currentResizer.classList.contains()) {
-            //     case "se" : 
-            //         //curs bas droite
-            //         elem.style.width = rect.width - (prevX - e.clientX) + "px";
-            //         elem.style.height = rect.height - (prevY - e.clientY) + "px";
-
-            //         break;
-            //     case "sw" : 
-            //         //curs bas gauche
-            //         elem.style.width = rect.width + (prevX - e.clientX) + "px";
-            //         elem.style.height = rect.height - (prevY - e.clientY) + "px";
-
-            //         elem.style.left = rect.left - (prevX - e.clientX) + "px";
-
-            //         break;
-            //     case "ne" : 
-            //         //curs haut droite
-            //         elem.style.width = rect.width - (prevX - e.clientX) + "px";
-            //         elem.style.height = rect.height + (prevY - e.clientY) + "px";
-
-            //         elem.style.top = rect.top - (prevY - e.clientY) + "px";
-
-            //         break;
-            //     case "nw" : 
-            //         //curs haut gauche
-            //         elem.style.width = rect.width + (prevX - e.clientX) + "px";
-            //         elem.style.height = rect.height + (prevY - e.clientY) + "px";
-
-            //         elem.style.top = rect.top - (prevY - e.clientY) + "px";
-            //         elem.style.left = rect.left - (prevX - e.clientX) + "px";
-
-            //         break;
-            // }
 
             prevX = e.clientX;
             prevY = e.clientY;
@@ -125,4 +197,53 @@ resizer.forEach(item => {
             isResizing = false;
         }
     }
+});
+
+//sélection de la div --------------------------------------------------------
+movable.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        // console.log(e);
+        // console.log(e.target.parentNode);
+        targP = e.target.parentNode;
+
+        if(!targP.classList.contains("selected")) {
+            targP.classList.add("selected", "depl");
+            addSelectedBtns(targP);
+            console.log(elem);
+
+        } else {
+            targP.classList.remove("selected", "depl");
+            remSelectedBtns(targP);
+        }
+        
+        function addSelectedBtns(parent) {
+            const newNe = document.createElement("div");
+            newNe.classList.add("resizer", "ne");
+
+            const newNw = document.createElement("div");
+            newNw.classList.add("resizer", "nw");
+
+            const newSw = document.createElement("div");
+            newSw.classList.add("resizer", "sw");
+
+            const newSe = document.createElement("div");
+            newSe.classList.add("resizer", "se");
+
+
+            parent.appendChild(newNe);
+            parent.appendChild(newNw);
+            parent.appendChild(newSw);
+            parent.appendChild(newSe);
+
+        }
+
+        function remSelectedBtns(parent) {
+            const resi = parent.querySelectorAll(".resizer");
+            console.log(resi);
+            resi.forEach((item) => { 
+                item.remove();
+            });
+        }
+    })
 })
+
