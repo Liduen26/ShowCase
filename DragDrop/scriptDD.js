54cd115c3d0a072e265695efe639ede611486c3e
Raphael <raphael.orieux@gmail.com>
2021-12-06 16:03:41 +0100

    Déplacement et resize sur grille

--- DragDrop/scriptDD.js
+++ DragDrop/scriptDD.js
@@ -12,6 +12,8 @@ Insérer le script dans votre body avec :
 Il faudra également ajouter le fichier styleDD.css avec ce lien dans le head : 
 <link rel="stylesheet" href="styleDD.css">
 
+/!\ EN cas de déplacemnt d'image, ajouter imgDD à la classe de l'image
+
 Problèmes : 
 - L'utilisation du css border peut provoquer un bug visuel avec les btn de resize
 => utiliser outline ?
@@ -23,22 +25,46 @@ const movable = document.querySelectorAll(".movable");
 let currentResizer;
 let isResizing = false;
 
-tGrid = "50";
+let tGrid = "5";
+let xGrid;
+let yGrid;
+
 // Ecoute pour le déplacement --------------------------------------------------
 document.body.addEventListener("mousedown", (e) => {
     console.log(e.target.parentNode);
     
     let targP = e.target;
     do {
+        console.log("iciii");
         if(targP.classList.contains("movable")) {
             //c'est bon
         } else {
             targP = targP.parentNode;
         }
-    } while(!targP.classList.contains("movable"));
 
-    if(e.target.classList.contains("movable") || targP.classList.contains("movable")) {
-        
+        if(targP === document.body) {
+            console.log("oui et +");
+            remClass("selected");
+            remClass("depl");
+
+            //désaffichage de la grille
+            const grid = document.body.querySelectorAll(".grid");
+            grid.forEach((item) => {
+                item.style.visibility = "hidden";
+            });
+            remSelectedBtns(document.body);
+            remDivDepl();
+            break;
+            
+        }
+        console.log(targP);
+    } while(!targP.classList.contains("movable") || targP == document.body);
+
+    if(e.target.classList.contains("depl") || targP.classList.contains("depl")) {
+        const zoneVisu = document.querySelector(".zoneSelect");
+        let newRleft;
+        let newRtop;
+
         window.addEventListener("mousemove", mousemove);
         window.addEventListener("mouseup", mouseup);
 
@@ -52,10 +78,43 @@ document.body.addEventListener("mousedown", (e) => {
                 let newX = prevX - e.clientX;
                 let newY = prevY - e.clientY;
 
+                const gridContainer = document.body.querySelector(".gridCont");
+                gridContainer.style.visibility = "visible";
+                remClass("inMove");
+                targP.classList.add("inMove");
+                    
+                //récup de la taille des cases de la grille
+                const grid = document.body.querySelectorAll(".grid");
+                const rectGrid = grid[0].getBoundingClientRect();
+                xGrid = rectGrid.width;
+                yGrid = rectGrid.height;
+
                 const rect = targP.getBoundingClientRect();
 
                 targP.style.left = rect.left - newX + 'px';
                 targP.style.top = rect.top - newY + 'px';
+                
+                let resteLeft = rect.left % xGrid;
+                
+                if(resteLeft > (xGrid / 2)) {
+                    let diff = xGrid - resteLeft;
+                    
+                    newRleft = rect.left + diff;
+                } else {
+                    newRleft = rect.left - resteLeft;
+                }
+                
+                let resteTop = rect.top % yGrid;
+                if(resteTop > (yGrid / 2)) {
+                    let diff = yGrid - resteTop;
+
+                    newRtop = rect.top + diff;
+                } else {
+                    newRtop = rect.top - resteTop;
+                }
+
+                zoneVisu.style.left = newRleft + "px";
+                zoneVisu.style.top = newRtop + "px";
 
                 //maj des coo de la souris
                 prevX = e.clientX;
@@ -64,6 +123,13 @@ document.body.addEventListener("mousedown", (e) => {
         }
 
         function mouseup() {
+            targP.style.left = newRleft + 'px';
+            targP.style.top = newRtop + 'px';
+
+            const gridContainer = document.body.querySelector(".gridCont");
+            gridContainer.style.visibility = "hidden";
+            remClass("inMove");
+
             //annulation de tout les eventListener de la souris
             window.removeEventListener("mousemove", mousemove);
             window.removeEventListener("mouseup", mouseup);
@@ -82,6 +148,10 @@ document.body.addEventListener("mousedown", (e) => {
         } else {
             targP = targP.parentNode;
         }
+
+        if(targP === document.body) {
+            break;
+        }
     } while(!targP.classList.contains("movable"));
 
     if(e.target.classList.contains("resizer") || targP.classList.contains("resizer")) {
@@ -89,9 +159,26 @@ document.body.addEventListener("mousedown", (e) => {
         currentResizer = e.target;
         isResizing = true;
 
+        const zoneVisu = document.querySelector(".zoneSelect");
+        let newRleft;
+        let newRtop;
+        let newRwidth;
+        let newRheight;
+
         //recup des coo intiales de la souris
         let prevX = e.clientX;
         let prevY = e.clientY;
+
+        //affichage de la grille
+        const gridContainer = document.body.querySelector(".gridCont");
+        gridContainer.style.visibility = "visible";
+        remClass("inMove");
+        targP.classList.add("inMove");
+
+        const grid = document.body.querySelectorAll(".grid");
+        const rectGrid = grid[0].getBoundingClientRect();
+        xGrid = rectGrid.width;
+        yGrid = rectGrid.height;
 
         window.addEventListener("mousemove", mousemove);
         window.addEventListener("mouseup", mouseup);
@@ -99,23 +186,72 @@ document.body.addEventListener("mousedown", (e) => {
         function mousemove(e) {
             const rect = targP.getBoundingClientRect();
 
+            let resteLeft = rect.left % xGrid;
+            if(resteLeft > (xGrid / 2)) {
+                let diff = xGrid - resteLeft;
+                
+                newRleft = rect.left + diff;
+            } else {
+                newRleft = rect.left - resteLeft;
+            }
+            
+            let resteTop = rect.top % yGrid;
+            if(resteTop > (yGrid / 2)) {
+                let diff = yGrid - resteTop;
+
+                newRtop = rect.top + diff;
+            } else {
+                newRtop = rect.top - resteTop;
+            }
+
+            let resteWidth = rect.width % xGrid;
+            if(resteWidth > (xGrid / 2)) {
+                let diff = xGrid - resteWidth;
+                
+                newRwidth = rect.width + diff;
+            } else {
+                newRwidth = rect.width - resteWidth;
+            }
+            
+            let resteHeight = rect.height % yGrid;
+            if(resteHeight > (yGrid / 2)) {
+                let diff = yGrid - resteHeight;
+
+                newRheight = rect.height + diff;
+            } else {
+                newRheight = rect.height - resteHeight;
+            }
+
+
             if(currentResizer.classList.contains("se")) {
                 //curs bas droite
                 targP.style.width = rect.width - (prevX - e.clientX) + "px";
                 targP.style.height = rect.height - (prevY - e.clientY) + "px";
 
+                //visuel
+                zoneVisu.style.width = newRwidth + "px";
+                zoneVisu.style.height = newRheight + "px";
             } else if(currentResizer.classList.contains("sw")) {
                 //curs bas gauche
                 targP.style.width = rect.width + (prevX - e.clientX) + "px";
                 targP.style.height = rect.height - (prevY - e.clientY) + "px";
 
                 targP.style.left = rect.left - (prevX - e.clientX) + "px";
+
+                //visuel
+                zoneVisu.style.width = newRwidth + "px";
+                zoneVisu.style.height = newRheight + "px";
+                zoneVisu.style.left = newRleft + "px";
             } else if(currentResizer.classList.contains("ne")) {
                 //curs haut droite
                 targP.style.width = rect.width - (prevX - e.clientX) + "px";
                 targP.style.height = rect.height + (prevY - e.clientY) + "px";
 
                 targP.style.top = rect.top - (prevY - e.clientY) + "px";
+
+                zoneVisu.style.width = newRwidth + "px";
+                zoneVisu.style.height = newRheight + "px";
+                zoneVisu.style.top = newRtop + "px";
             } else if(currentResizer.classList.contains("nw")) {
                 //curs haut gauche
                 targP.style.width = rect.width + (prevX - e.clientX) + "px";
@@ -123,6 +259,11 @@ document.body.addEventListener("mousedown", (e) => {
 
                 targP.style.top = rect.top - (prevY - e.clientY) + "px";
                 targP.style.left = rect.left - (prevX - e.clientX) + "px";
+
+                zoneVisu.style.width = newRwidth + "px";
+                zoneVisu.style.height = newRheight + "px";
+                zoneVisu.style.left = newRleft + "px";
+                zoneVisu.style.top = newRtop + "px";
             }
 
             //maj des coo de la souris
@@ -131,6 +272,16 @@ document.body.addEventListener("mousedown", (e) => {
         }
         
         function mouseup() {
+            targP.style.width = newRwidth + "px";
+            targP.style.height = newRheight + "px";
+            targP.style.left = newRleft + "px";
+            targP.style.top = newRtop + "px";
+
+            //désaffichage de la grille
+            const gridContainer = document.body.querySelector(".gridCont");
+            gridContainer.style.visibility = "hidden";
+            remClass("inMove");
+
             //annulation de tout les eventListener de la souris
             window.removeEventListener("mousemove", mousemove);
             window.removeEventListener("mouseup", mouseup);
@@ -149,29 +300,34 @@ movable.forEach((item) => {
             } else {
                 targP = targP.parentNode;
             }
+
+            if(targP == document.body) {
+                
+            }
+            console.log(targP);
         } while(!targP.classList.contains("movable"));
         
         if(!targP.classList.contains("selected")) {
             //on retire les autres sélections
             remClass("selected");
+            remClass("depl");
             remSelectedBtns(document.body);
 
             //On met la sélection sur ce qui nous intéresse
-            targP.classList.add("selected");
+            targP.classList.add("selected", "depl");
             addSelectedBtns(targP);
 
-            const grid = document.body.querySelectorAll("grid");
-            console.log(grid);
-            grid.forEach((item) => {
-                item.style.visibility = "visible";
-                console.log(item);
-            });
+            remDivDepl();
+            const rect = targP.getBoundingClientRect();
+            const parentTarg = targP.parentNode;
+            createDivDepl(parentTarg, rect);
 
         } else {
             //si la div contient déjà selected, on l'enlève
             targP.classList.remove("selected");
+            targP.classList.remove("depl");
             remSelectedBtns(targP);
-
+            remDivDepl();
         }
         
         function addSelectedBtns(parent) {
@@ -194,38 +350,65 @@ movable.forEach((item) => {
             parent.appendChild(newSe);
         }
 
-        function remSelectedBtns(parent) {
-            //retire les btns de resize
-            const resi = parent.querySelectorAll(".resizer");
-            resi.forEach((item) => { 
-                item.remove();
-            });
+        
+        function createDivDepl(parent, rect) {
+            const newDivDepl = document.createElement("div");
+            newDivDepl.classList.add("zoneSelect");
+            parent.appendChild(newDivDepl);
+            
+            const div = document.querySelector(".zoneSelect");
+            div.style.left = rect.left + "px";
+            div.style.width = rect.width + "px";
+            div.style.top = rect.top + "px";
+            div.style.height = rect.height + "px";
         }
+        
+    });
+});
 
-        function remClass(classToRem) {
-            const cl = document.body.querySelectorAll("."+classToRem);
-            cl.forEach((item) => {
-                item.classList.remove(classToRem);
-            });
-        }
-    })
-})
+
+//fonctions d'automatisation -----------------------------------------------
+function remSelectedBtns(parent) {
+    //retire les btns de resize
+    const resi = parent.querySelectorAll(".resizer");
+    resi.forEach((item) => { 
+        item.remove();
+    });
+}
+
+function remClass(classToRem) {
+    const cl = document.body.querySelectorAll("."+classToRem);
+    cl.forEach((item) => {
+        item.classList.remove(classToRem);
+    });
+}
+
+function remDivDepl() {
+    const div = document.querySelectorAll(".zoneSelect");
+    console.log(div);
+    div.forEach((item) => {
+        item.remove();
+    });
+}
 
 
 document.onload = loading();
-
 function loading() {
     const newCont = document.createElement("div");
     newCont.classList.add("gridCont");
     document.body.appendChild(newCont);
     
     const container = document.querySelector(".gridCont");
-    console.log(container);
-    for(let i = 0;i < 400;i++) {
+    for(let i = 0;i < 300;i++) {
         const newGrid = document.createElement("div");
-        newGrid.style.width = tGrid + "px";
-        newGrid.style.height = tGrid + "px";
+        newGrid.style.width = tGrid + "vw";
+        newGrid.style.height = tGrid + "vh";
         newGrid.classList.add("grid");
+
+        const newGridInt = document.createElement("div");
+        newGridInt.classList.add("gridInt");
+        newGrid.appendChild(newGridInt);
+
         container.appendChild(newGrid);
     }
     
