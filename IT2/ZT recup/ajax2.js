958a6e6e03621902f00c96b0ce62f28f5b70263b
Paul <devred@et.intechinfo.fr>
2022-01-14 14:38:43 +0100

    suppression fichiers

--- IT2/ZT recup/ajax2.js
+++
@@ -1,2 +0,0 @@
-    document.write('<input type="hidden" name="info" value="' + mavariablejs + '">'); 
-<input type="submit" value="valider">
--- IT2/ZT recup/controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-        if(isset($_POST['module']) && isset($_POST['method'])){
-			$Module = $_POST['module'];
-			$Method = $_POST['method'];
-            echo $module;
-        }
-	?>
