826d6a4568b97e0355d7120bbe91d4065e4ae7b1
Robin <rpalmierobin@outlook.fr>
2022-01-17 10:47:26 +0100

    push

--- outil/scripts/script_insert.js
+++ outil/scripts/script_insert.js
@@ -1,5 +1,6 @@
 const btn1 = document.querySelector('#btn_a');
 const btn2 = document.querySelector('#btn_b');
+const btn2_5 = document.querySelector('#btn_b_2');
 const btn3 = document.querySelector('#btn_c');
 const btn4 = document.querySelector('#btn_d');
 const list = document.getElementById('list');
@@ -7,7 +8,7 @@ const form = document.querySelector('form');
 const item = document.getElementById('item');
 const cont = document.querySelectorAll('.container');
 const text = document.querySelectorAll('.text');
-const div_parent = document.querySelector('.page');
+const div1_parent = document.querySelector('.page');
 let nbr_id = 0;
 console.log(nbr_id);
 
@@ -23,7 +24,7 @@ btn1.addEventListener('click', () => {
   var newContent = document.createTextNode('inserer text')
   new_p.appendChild(newContent);
   new_dt.appendChild(new_p);
-  div_parent.appendChild(new_dt);
+  div1_parent.appendChild(new_dt);
   nbr_id = nbr_id + 1;
   for (let i = 0; i < 100; i++) {
     new_dt.setAttribute("id", nbr_id); 
@@ -34,17 +35,21 @@ btn1.addEventListener('click', () => {
 // //zone image //
 
 
-btn2.addEventListener('click', () => {
+btn2_5.addEventListener('click', () => {
+  
+  console.log("test btn2.5");
+  
+  var new_di = document.getElementById("image");
 
-  const new_di = document.createElement('div');
-  new_di.setAttribute("class","image movable");
-  new_di.setAttribute("style",'background: url("Dream.jpg") center/cover; position: absolute; left: -10vw; top: 10vh; width: 100vw; height: 100vh;');
-  div_parent.appendChild(new_di);
-  nbr_id = nbr_id + 1;
-  for (let i = 0; i < 100; i++) {
-    new_di.setAttribute("id", nbr_id); 
+
+  // new_di.setAttribute("style",'center/cover; position: absolute; left: -10vw; top: 10vh; width: 50vw; height: 50vh;');
+  // new_di.setAttribute("class","image movable");
+
+  // nbr_id = nbr_id + 1;
+  // for (let i = 0; i < 100; i++) {
+  //   new_di.setAttribute("name", nbr_id); 
     
-  }
+  // }
 });
 
 
@@ -54,7 +59,7 @@ btn3.addEventListener('click', () => {
 
   const new_df = document.createElement('div');
   new_df.setAttribute("class"," forme exemple movable");
-  div_parent.appendChild(new_df);
+  div1_parent.appendChild(new_df);
 
   nbr_id = nbr_id + 1;
   for (let i = 0; i < 100; i++) {
@@ -74,7 +79,7 @@ console.log("click");
   const new_db = document.createElement('button');
   new_db.setAttribute("class"," boutton movable"); 
   new_dd.appendChild(new_db);
-  div_parent.appendChild(new_dd);
+  div1_parent.appendChild(new_dd);
 
   nbr_id = nbr_id + 1;
   for (let i = 0; i < 100; i++) {
