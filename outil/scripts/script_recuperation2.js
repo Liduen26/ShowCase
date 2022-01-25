9cd638cd2ae4328e8f95841ef4944f3f49c0e83f
Paul <devred@et.intechinfo.fr>
2022-01-25 12:47:51 +0100

    recup sections fonctionnel
    le script_recuperation2.js recupere maintenant les sections et les les elements du site.

--- outil/scripts/script_recuperation2.js
+++ outil/scripts/script_recuperation2.js
@@ -1,23 +1,25 @@
 const btn_save = document.querySelector("#btn_save");
 btn_save.addEventListener("click", recup);
-    
-function recup () {
-    elements = document.querySelectorAll('#page .movable');
+
+function recup() {
+    elements = document.querySelectorAll('.page .element');
     let page = {};
     console.log(elements.length);
 
-    for(let i=0; i<elements.length; i++){
+    for (let i = 0; i < elements.length; i++) {
         page[i] = elements[i].outerHTML;
     }
-    test = elements[1].outerHTML;
+    // test = elements[1].outerHTML;
+    // console.log(page);
 
     $.ajax({
-        url:"./scripts/lireJson.php",
+        url: "./scripts/lireJson.php",
+
         method: "POST",
         data: page,
         // datatype: "html",
         // processData: false,
-        success: function(res) {
+        success: function (res) {
             console.log(res);
         }
     })
